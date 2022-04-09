80f30d8adb83104008f3da424a11edd94e9da8b0
invo12 <alex_ilioi@yahoo.com>
2022-04-09 01:25:36 -0400

    Add capture of pieces

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -21,20 +21,15 @@ class Game(private val graphics: Graphics, private val pieces: MutableList<Piece
         "K" to KingRule(),
     )
 
-    private fun getPositions(piece: Piece): Pair<List<Position>, List<Position>> {
+    private fun getMoves(piece: Piece): Pair<List<Position>, List<Position>> {
 
-        fun String.matchCase(other: String): Boolean {
-            val c: Char = this[0]
-            val oc: Char = other[0]
-            return (c.isLowerCase() && oc.isLowerCase()) || (c.isUpperCase() && oc.isUpperCase())
-        }
 
         fun getFriendlyPositions(): List<Position> {
-            return pieces.filter { it.getType().matchCase(piece.getType()) }.map { it.getPosition() }
+            return pieces.filter { it.hasTheSameColor(piece) }.map { it.getPosition() }
         }
 
         fun getEnemyPositions(): List<Position> {
-            return pieces.filter { !it.getType().matchCase(piece.getType()) }.map { it.getPosition() }
+            return pieces.filter { !it.hasTheSameColor(piece) }.map { it.getPosition() }
         }
 
         return rules[piece.getType()]?.getValidPositions(
@@ -51,22 +46,44 @@ class Game(private val graphics: Graphics, private val pieces: MutableList<Piece
     fun notify(x: Int, y: Int) {
 
         val piece = pieces.find { it.getPosition() == Position(x, y) }
-        if (piece != null) {
-            selectedPiece = piece
-            graphics.showMoves(getPositions(piece).first + getPositions(piece).second)
-        } else {
-            val movePositions = selectedPiece?.let {
-                getPositions(it).first + getPositions(it).second
-            }
-            val positionToMove = movePositions?.find { it.x == x && it.y == y }
-            if (positionToMove == null) {
-                graphics.showMoves(listOf())
+        if (selectedPiece != null) {
+            if (piece != null) {
+                if (piece.hasTheSameColor(selectedPiece!!)) {
+                    selectedPiece = piece
+                    val moves = getMoves(piece)
+                    graphics.showMoves(moves.first + moves.second)
+                } else {
+                    val (_, captures) = getMoves(selectedPiece!!)
+                    val capture = captures.find { it == Position(x, y) }
+                    if (capture != null) {
+                        pieces.remove(piece)
+                        selectedPiece!!.move(Position(x, y))
+                        graphics.updatePieces(pieces)
+                        graphics.showMoves(listOf())
+                        selectedPiece = null
+                    }
+                }
             } else {
-                selectedPiece?.getPosition()?.x = x
-                selectedPiece?.getPosition()?.y = y
-                graphics.updatePieces(pieces)
+                val movePositions = selectedPiece?.let {
+                    val moves = getMoves(it)
+                    moves.first + moves.second
+                }
+                val positionToMove = movePositions?.find { it.x == x && it.y == y }
+                if (positionToMove == null) {
+                    graphics.showMoves(listOf())
+                } else {
+                    selectedPiece?.getPosition()?.x = x
+                    selectedPiece?.getPosition()?.y = y
+                    graphics.updatePieces(pieces)
+                }
+                selectedPiece = null
             }
-            selectedPiece = null
+        } else {
+            if (piece != null) {
+                selectedPiece = piece
+                val moves = getMoves(piece)
+                graphics.showMoves(moves.first + moves.second)
+            }
         }
     }
 
--- src/main/kotlin/pieces/Piece.kt
+++ src/main/kotlin/pieces/Piece.kt
@@ -1,17 +1,26 @@
 package pieces
 
+fun String.matchCase(other: String): Boolean {
+    val c: Char = this[0]
+    val oc: Char = other[0]
+    return (c.isLowerCase() && oc.isLowerCase()) || (c.isUpperCase() && oc.isUpperCase())
+}
+
 class Piece(private val pieceInfo: PieceInfo) {
 
     fun getPosition(): Position {
         return pieceInfo.position
     }
 
-    fun draw() {}
     fun move(position: Position) {
         pieceInfo.position = position
     }
 
     fun getType(): PieceType {
         return pieceInfo.type
+    }
+
+    fun hasTheSameColor(p: Piece): Boolean {
+        return this.getType().matchCase(p.getType())
     }
 }
